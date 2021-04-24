2cc24c165203a3f7d9bf9a836ef3bc5cf8a44747
AdrienHq <adrienhecq@gmail.com>
2021-04-24 20:25:55 +0200

    feat: alot

---
+++ src/Controller/Admin/AdminUserController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Form\RegistrationType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class AdminUserController extends AbstractController
+{
+    /**
+     * @var UserRepository
+     */
+    private $repository;
+
+    public function __construct(UserRepository $repository, EntityManagerInterface $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/adminUser", name="admin.user.index")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index()
+    {
+        $user = $this->repository->findAll();
+        return $this->render('admin/user/index.html.twig', compact('user'));
+    }
+
+    /**
+     * @Route("/admin/user/create", name="admin.user.new")
+     */
+    public function new(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $this->em->persist($user);
+            $this->em->flush();
+            $this->addFlash('success', 'Creation successful');
+            return $this->redirectToRoute('admin.user.index');
+        }
+        return $this->render('admin/user/new.html.twig', [
+            'user' => $user,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/user/{id}", name="admin.user.edit", methods="GET|POST")
+     */
+    public function edit(User $user, Request $request)
+    {
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $this->em->flush();
+            $this->addFlash('success', 'Edit successful');
+            return $this->redirectToRoute('admin.user.index');
+        }
+        return $this->render('admin/user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView()
+        ]);
+    }
+
+     /**
+     * @Route("/admin/user/{id}", name="admin.user.delete", methods="DELETE")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function delete(Request $request, User $user)
+    { 
+        if($this->isCsrfTokenValid('delete'.$user->getId(), $request->get('_token'))) {
+            $this->em->remove($user);
+            $this->em->flush();
+            $this->addFlash('success', 'Deletion successful');
+            return $this->redirectToRoute('admin.user.index');
+        }
+        return $this->redirectToRoute('admin.user.index');
+    }
+}
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Doctrine\ORM\EntityManagerInterface;
 
 class SecurityController extends AbstractController
@@ -41,12 +42,32 @@ class SecurityController extends AbstractController
    /**
      * @Route("/inscription", name="inscription")
      */
-    public function registration(Request $request)
+    public function registration(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
         $user = new User();
         $form = $this->createForm(RegistrationType::class, $user);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $brm = ( ($user->getWeight()*10)+($user->getHeight()*6.25)-(5*$user->getAge())+5);
+            if( $user->getGender() == 0 ){
+                $brm += 5;
+            } else {
+                $brm -= 161;
+            }
+            if($user->getActivity() == 0){
+                $brm *= 1.2;
+            } 
+            if( $user->getActivity() == 1){
+                $brm *= 1.55;
+            }
+            if( $user->getActivity() == 2){
+                $brm *= 1.725;
+            }  
+            if( $user->getActivity() == 3){
+                $brm *= 1.9;
+            }
+            $user->setBrm($brm);
             $this->em->persist($user);
             $this->em->flush();
             $this->addFlash('success', 'Creation successful');
